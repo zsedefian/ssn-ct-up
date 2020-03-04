12a0292dbf693d38cbf8b31224e54e008180d5ec
Zachary Sedefian <zach@nuvalence.io>
2020-03-04 15:23:38 -0500

    Add javadoc to user credential service

--- src/main/java/services/UserCredentialsService.java
+++ src/main/java/services/UserCredentialsService.java
@@ -5,8 +5,17 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import models.UserCredentials;
 import org.json.JSONObject;
 
+/**
+ * Determines user credentials.
+ */
 public class UserCredentialsService {
-    
+
+    /**
+     * Decodes JWT token in order to parse cognito username and phone number of user.
+     *
+     * @param jwtToken JWT token
+     * @return User credentials object
+     */
     public UserCredentials getUserCredentials(String jwtToken) {
         DecodedJWT decodedJwt = JWT.decode(jwtToken);
         String payload = decodedJwt.getPayload();
